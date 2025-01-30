deb1f97211b9c0f6d053121cc1eb8d6a5a707108
LeideMarina18 <leidemarina19@gmail.com>
2025-01-29 21:34:07 -0400

    TASK: Implemente uma função para adicionar amigos

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = []
+let nome = '';
+
+function adicionarAmigo(){ //Função que adiciona os amigos na lista
+    nome = document.querySelector('input').value; //Capturar o valor do campo de entrada
+    if(nome != ''){ //Validar a entrada: Verifica se a entrada não é uma string vazia
+        amigos.push(nome); //Atualizar array de amigos: insere na lista o nome com o métod push
+        limparCampo(); //Limpar o campo de entrada
+        console.log(amigos);
+    }else{
+        alert('Por favor, insira um nome'); //Alerta de campo vazio
+    }
+}
+
+function limparCampo() {
+    nome = document.querySelector('input');
+    nome.value = '';
+}
